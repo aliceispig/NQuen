ca32a83c61fcbb89eb050a7b5d298a688815bd0a
root <root@gmail.com>
2022-12-21 11:05:07 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # NQuen
 for interview test
+
+
+#test
+./gradle test
+
+#run
+./gradlew run
+
+#build
+./gradlew jar
+
+
---
+++ src/main/java/com/test/eQueen/EightQueen.java
@@ -0,0 +1,23 @@
+package com.test.eQueen;
+
+public class EightQueen {
+
+    private int n = 0;
+    private int sizeW=0;
+    private int sizeH=0;
+
+    /**
+     * init Game
+     * @param n Queen number
+     * @param sizeW
+     * @param sizeH
+     */
+    public void EightQueen(int n , int sizeW  ,int sizeH){
+        this.n = n;
+        this.sizeW = sizeW;
+        this.sizeH = sizeH;
+    }
+
+
+
+}
