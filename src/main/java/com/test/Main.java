128feb3331df1f0d271a197d12d6fb23a132df9b
aliceispig <aliceispig@gmail.com>
2022-12-23 21:05:14 +0800

    udpate

--- src/main/java/com/test/Main.java
+++ src/main/java/com/test/Main.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args){
+        int queenNum =Integer.valueOf( args[0] );
 
-        EightQueen a= new EightQueen(1,1);
+        int chessSize = Integer.valueOf(args[1]);
+
+        EightQueen a= new EightQueen(queenNum,chessSize);
 
         a.solve(0);
 
